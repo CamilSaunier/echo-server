69b060f1a00582c750a24f46b968df93fb4d6005
Camil Saunier <camilsaunier@gmail.com>
2026-09-07 14:55:42 +0200

    add methods PATCH in cors middleware

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -4,7 +4,7 @@ const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
 export const corsMiddleware = cors({
   origin: FRONTEND_URL,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 });
